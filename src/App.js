0ee5916a94e54b5d3f64eaca97874dc8596cba90
Jeremiah <jeremiah3643@gmail.com>
2018-06-11 13:34:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,141 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from './nav/NavBar';
+import Register from "./login/Register";
+import Login from './login/Login';
 
 class App extends Component {
+  // Set initial state
+  state = {
+    currentView: "login",
+    searchTerms: "",
+    activeUser: localStorage.getItem("bandId"),
+    newEmail: "",
+    newPassword: ""
+  };
+
+  // Search handler -> passed to NavBar
+  
+    // Set initial state
+    state = {
+        currentView: "login",
+        searchTerms: "",
+        activeUser: localStorage.getItem("bandId"),
+        viewingUser: ""
+    }
+
+    performSearch = function(terms) {
+      this.setState({
+        searchTerms: terms,
+        currentView: "results"
+      });
+    }.bind(this);
+
+  // Set Username/password field to newly created username and password
+  setUsernamePassword = function (newUsername, newPassword) {
+    this.setState({
+      newEmail: newUsername,
+      newPassword: newPassword
+    })  
+    
+  }.bind(this)
+
+  // Function to update local storage and set activeUser state
+  setActiveUser = val => {
+    if (val) {
+      localStorage.setItem("bandId", val);
+      this.setState({
+        activeUser: val
+      })
+    } else {
+      localStorage.removeItem("bandId");
+      this.setState({
+        activeUser: null
+      })
+    }
+  }
+
+    
+    setViewingUser = function (val) {
+        this.setState({
+            viewingUser: val
+        })
+    }.bind(this)
+
+
+  // View switcher -> passed to NavBar and Login
+  // Argument can be an event (via NavBar) or a string (via Login)
+  showView = function(e) {
+    let view = null;
+
+    // Click event triggered switching view
+    if (e.hasOwnProperty("target")) {
+      view = e.target.id.split("__")[1];
+
+      // View switch manually triggered by passing in string
+    } else {
+      view = e;
+    }
+
+    // If user clicked logout in nav, empty local storage and update activeUser state
+    if (view === "logout") {
+      this.setActiveUser(null);
+    }
+
+    // Update state to correct view will be rendered
+    this.setState({
+      currentView: view
+    });
+  }.bind(this);
+
+  /*
+        Function to determine which main view to render.
+
+        TODO:
+            2. Register view
+            3. Create event view
+    */
+  View = () => {
+    if (this.state.currentView === "register") {
+      return (
+        <Register showView={this.showView} setActiveUser={this.setActiveUser} setUsernamePassword={this.setUsernamePassword}/>
+      );
+    } else if (localStorage.getItem("bandId") === null) {
+      return (
+        <Login showView={this.showView} setActiveUser={this.setActiveUser} newEmail={this.state.newEmail} newPassword={this.state.newPassword}/>
+      );
+    } else {
+      switch (this.state.currentView) {
+        case "logout":
+          return (
+            <Login
+              showView={this.showView}
+              setActiveUser={this.setActiveUser}
+              newEmail={this.newEmail} 
+              newPassword={this.newPassword}
+            />
+          );
+      }
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <article>
+        <NavBar
+          viewHandler={this.showView}
+          searchHandler={this.performSearch}
+          activeUser={this.state.activeUser}
+          setActiveUser={this.setActiveUser}
+        />
+
+        {this.View()}
+      </article>
     );
   }
 }
+
 
 export default App;
